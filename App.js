c6cdec900f2f5561bd91a14c02533fa20a8c9bf9
Brenno Luis <devblsds@gmail.com>
2018-09-18 01:23:03 -0300

    Daily phrases generator

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import { 
-  StyleSheet, 
-  Image, 
+import {
+  StyleSheet,
+  Image,
   View,
   TouchableOpacity,
-  Text, 
+  Text,
 } from 'react-native';
 
 const img = require('./imgs/logo.png');
+
+const showPhrase = () => {
+  randomNumber = Math.floor(Math.random() * 5);
+  const phrases = Array();
+
+  phrases[0] = 'With the new day comes new strength and new thoughts.'; 
+  phrases[1] = 'With the new day comes new strength and new thoughts.';
+  phrases[2] = 'Beauty is power; a smile is its sword.';
+  phrases[3] = 'The past cannot be changed. The future is yet in your power.';
+  phrases[4] = 'You will never win if you never begin.';
+  
+  alert(phrases[randomNumber]);
+};
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.main}>
         <Image source={img} />
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity onPress={showPhrase} style={styles.button}>
           <Text style={styles.buttonText}>New phrase</Text>
         </TouchableOpacity>
       </View>
